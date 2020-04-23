c6a970b7c9e1f98aaf1c7dfe0eafe3b7aedc7250
Allison West <allisonwest@Allisons-MBP.lan>
2020-04-23 17:20:01 -0500

    final final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,7 +3,6 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputField1 = d3.select("#datetime");
-var inputField2 = d3.select("#city");
 var resetbtn = d3.select("#reset-btn");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
